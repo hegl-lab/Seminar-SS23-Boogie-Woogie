dba43faa10c1f71a7c8abb0ef489079f0f2a9853
Arjan Siddhpura <arjan.siddhpura@stud.uni-heidelberg.de>
2023-06-23 23:56:56 +0200

    Updated index.html to include css and fixed window width and height

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight - 15);
+    createCanvas(windowWidth, windowHeight);
     randomSeed(int(random(10000000)));
     victory = new Canvas(width / 2, height / 2);
     victory.setup();
